752571e611476603f82c7440489d39fcba1b358e
mVang <meng.vang92@yahoo.com>
2018-06-19 12:56:46 -0500

    Trivia Game Update

--- TriviaGame-/assets/javascript/app.js
+++ TriviaGame-/assets/javascript/app.js
@@ -1,119 +1,120 @@
-$(document).ready(function(){
-    var count = 5;
-    var score;
-    var timerOn = false;
-    $(".welcome").append('<h2 style="text-align:center">Welcome! Press start to play!</h2>');
-    $("#startButton").append('<button style="" type="button" class="btn btn-danger btn-lg">Start</button>');
-    $(".trivia").hide();
-    $("#nextButton").hide();
-    $(".nextBox").hide();
-    $(".triviaBox").hide();
+var currentQuestion; 
+var correctAnswer; 
+var incorrectAnswer; 
+var unanswered; 
+var seconds; 
+var time; 
+var answered; 
+var userSelect;
+var messages = {
+	correct: "Yes, that's right!",
+	incorrect: "No, that's not it.",
+	endTime: "Out of time!",
+	finished: "Alright! Let's see how well you did."
+}
 
-    $("#startButton").click(function(){
-        $("#nextButton").append('<button style="" type="button" class="btn btn-danger btn-lg">Next Question</button>');
-        $("#startButton").hide();
-        $("#nextButton").fadeIn();
-        $(".triviaBox").fadeIn();
-        $(".nextBox").fadeIn();
-        $(".trivia").fadeIn();
-        $(".welcome").html('<h2 style="text-align:center">Question 1</h2>')
-        // Start the game
-        running();
-    });
-
-    function running(){
-        //Total number of questions
-        // var totalNumQuestions = $(".questions");
-        //Display current question, sets it at first question
-        var currentQuestion = 0;
-        //jQuery variable
-        $questions = $(".questions");
-        //Hide all of the questions
-        $questions.hide();
-        //Show the first question
-        $($questions.get(currentQuestion)).fadeIn();
-        timer()
-        //Click listener to get next question
-        $("#nextButton").click(function() {
-            $("#nextButton").hide();
-            timer()
-            //Current question disappears
-            $($questions.get(currentQuestion)).fadeOut(function() {
-                //Increment current question by 1. 
-                currentQuestion++;
-                // console.log(currentQuestion)
-                // Parse to int to prevent cascade
-                $(".welcome").html('<h2 style="text-align:center">Question ' + parseInt(currentQuestion  + 1) + '</h2>')
-                // Reset the clock
-                count = 5;
-                $($questions.get(currentQuestion)).fadeIn();
-                $("#nextButton").fadeIn();
-                console.log("total score" + score)
-            });
-        });
-    
-        // Scoring function
-        function setScore() {
-            score = 0;
-            // score = $("input:checked[value=right]").length;
-
-            if ($("input:checked[value=right]").length){
-                score++
-            console.log(score);
-            }
-        }
-
-        // Starts timer
-        function timer(){
-            function timedCount() {
-                $("#timer").html('<h2 style="text-align:center">' + count + '</h2>');
-
-                if (count === -1){
-                    $("#timer").html('<h2 style="text-align:center">Time is up!</h2>');
-                    $($questions.get(currentQuestion)).fadeOut(function() {
-                        //Increment current question by 1. 
-                        currentQuestion++;
-                        // console.log(currentQuestion)
-                        // Parse to int to prevent cascade
-                        $(".welcome").html('<h2 style="text-align:center">Question ' + parseInt(currentQuestion  + 1) + '</h2>')
-                        // Reset the clock
-                        count = 5;
-                        $($questions.get(currentQuestion)).fadeIn();
-                        $(".nextButton").fadeIn();
-                        
-                        // if (currentQuestion == 3){
-                        //     console.log(currentQuestion)
-                        // }
-                        setTimeout(timer, 1000)
-                    });
-
-                    count = 0;
-                    stopCount();
-
-                } else {
-                    count -= 1;
-                    time = setTimeout(timedCount, 1000);
-                }
-            }
-                    
-            function startCount() {
-                if (!timerOn) {
-                    timerOn = true;
-                    timedCount();
-                }
-            }
-
-            function stopCount() {
-                console.log("stop count")
-                timerOn = false;
-                count = 0;
-            }
-        // Begin timer
-        startCount();
-        }
-    //Set score when user chooses right choice
-    $("input").change(setScore);
-    //Set score when initialized
-    setScore();
-    }
+$("#startBtn").on("click", function(){
+	$(this).hide();
+	running();
 });
+
+$("#startOverBtn").on("click", function(){
+	$(this).hide();
+	running();
+});
+
+function running(){
+	$("#finalMessage").empty();
+	$("#correctAnswers").empty();
+	$("#incorrectAnswers").empty();
+	$("#unanswered").empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+	newQuestion();
+}
+
+function newQuestion(){
+	$("#message").empty();
+	$("#correctedAnswer").empty();
+	answered = true;
+    
+	$("#currentQuestion").html("Question #" + (currentQuestion + 1 ) + "/" + questions.length);
+	$(".question").html("<h2>" + questions[currentQuestion].question + "</h2>");
+	for(var i = 0; i < 4; i++){
+		var choices = $("<div>");
+		choices.text(questions[currentQuestion].answerList[i]);
+		choices.attr({"data-index": i });
+		choices.addClass("thisChoice");
+		$(".answerList").append(choices);
+    }
+    
+	countdown();
+	$(".thisChoice").on("click",function(){
+		userSelect = $(this).data("index");
+		clearInterval(time);
+		showAnswer();
+	});
+}
+
+function countdown(){
+	seconds = 15;
+	$("#timer").html("<h2>Time: " + seconds + "</h2>");
+	answered = true;
+	time = setInterval(showCountdown, 1000);
+}
+
+function showCountdown(){
+	seconds--;
+	$("#timer").html("<h2>Time: " + seconds + "</h2>");
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		showAnswer();
+	}
+}
+
+function showAnswer(){
+	$("#currentQuestion").empty();
+	$(".thisChoice").empty();
+	$(".question").empty();
+
+    var rightAnswerText = questions[currentQuestion].answerList[questions[currentQuestion].answer];
+    var rightAnswerIndex = questions[currentQuestion].answer;
+    
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$("#message").html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$("#message").html(messages.incorrect);
+		$('#correctedAnswer').html("The correct answer was: " + rightAnswerText);
+	} else{
+		unanswered++;
+		$("#message").html(messages.endTime);
+		$("#correctedAnswer").html("The correct answer was: " + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (questions.length-1)){
+		setTimeout(scoreboard, 5000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 5000);
+	}	
+}
+
+function scoreboard(){
+	$("#timer").empty();
+	$("#message").empty();
+	$("#correctedAnswer").empty();
+	$("#gif").empty();
+	$("#finalMessage").html(messages.finished);
+	$("#correctAnswers").html("Correct Answers: " + correctAnswer);
+	$("#incorrectAnswers").html("Incorrect Answers: " + incorrectAnswer);
+	$("#unanswered").html("Unanswered: " + unanswered);
+	$("#startOverBtn").addClass("reset");
+	$("#startOverBtn").show();
+	$("#startOverBtn").html("Start Over?");
+}
---
+++ TriviaGame-/assets/javascript/question.js
@@ -0,0 +1,28 @@
+var questions = [{
+	question: "Suppose there is an alien civilization 66 million light-years away from Earth. What would they see if they looked at the Earth right now?",
+	answerList: [
+        "The Dinosaurs",
+        "The Roman Empire",
+        "An ocean planet",
+        "The Incredibles"
+        ],
+	answer: 0
+},{
+	question: "What is the closest star system to our solar system?",
+	answerList: [
+        "Betelgeuse",
+        "Sirius",
+        "Alpha Centauri",
+        "The Moon"
+        ],
+	answer: 2
+},{
+	question: "The largest moon in our solar system belongs to which one of these planets?",
+	answerList: [
+        "Gliese 581",
+        "Saturn",
+        "Jupiter",
+        "Neptune"
+        ],
+	answer: 2
+}];
