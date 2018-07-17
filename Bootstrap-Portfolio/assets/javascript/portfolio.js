06b295b8fb52813a432fcaff6a93b2fb67ea5ceb
unknown <mVang92@github.com>
2018-07-17 01:34:51 -0500

    Portfolio Links updated

--- Bootstrap-Portfolio/assets/javascript/portfolio.js
+++ Bootstrap-Portfolio/assets/javascript/portfolio.js
@@ -1,34 +1,28 @@
-var project1ModalContent = document.getElementById("project1ModalContent");
-var project2ModalContent = document.getElementById("project2ModalContent");
-var project1Modal = document.getElementById("project1Modal");
-var project2Modal = document.getElementById("project2Modal");
+$(document).ready(function () {
+    var portfolioDesc = ("<p>To visit the application websites, click on the project icons."
+        + " Each project also has a short desciption and a link to their Github repositories.</p>")
 
-project1Modal.onclick = function () {
-    project1ModalContent.style.display = "block";
-}
+    // $("#portfolioDesc").html(portfolioDesc);
 
-project2Modal.onclick = function () {
-    project2ModalContent.style.display = "block";
-}
+    $("#project1").html("The Crystals Collector Game utilizes Javascript and jQuery code. A random number"
+        + " is given to the user at the start of each round, and the user must use the four crystals to add"
+        + " their total to equal the random number chosen. Each crystal will randomize every round"
+        + ", so keep track of the number behind the crystals.");
 
-window.onclick = function (event) {
-    if (event.target == project1ModalContent) {
-        project1ModalContent.style.display = "none";
-    }
-}
+    $("#project2").html("The Word Guess Game is based around automotive terminology and utilizes vanilla Javascript. The application"
+        + " will give a hint to the user and the user must use the keyboard to fill in the blanks for the chosen word.");
 
-window.onclick = function (event) {
-    if (event.target == project2ModalContent) {
-        project2ModalContent.style.display = "none";
-    }
-}
+    $("#project3").html("This application utilizes a combination of Javascript, jQuery, and an API. The user must answer a"
+        + " series of astronomy/physics based questions in a limited amount of time. Scores are displayed"
+        + " at the end, and the user may replay the game if they want.");
 
-$("#project1Close").on("click", function (evt) {
-    evt.stopImmediatePropagation();
-    project1ModalContent.style.display = "none";
-})
+    $("#project4").html("GifTastic was built around Javascript, jQuery, and API calls. This application appends new buttons"
+        + " next to the predefined buttons and allows the user to click and generate 10 Gif images at a time."
+        + " The application talks to an API, which returns the appropriate search results as searched. The"
+        + " Gif images are initially still, but once clicked, they become animated.");
 
-$("#project2Close").on("click", function (evt) {
-    evt.stopImmediatePropagation();
-    project2ModalContent.style.display = "none";
+    $("#project5").html("Train Scheduler utilizes the Moment.js library along with a Firebase database to store data on travel information. The application"
+        + " will take in several parameters and store them in the database. It will also calculate the next"
+        + " train arrival. Then it captures a snapshot from the database and displays the data back to the"
+        + " user.");
 })
