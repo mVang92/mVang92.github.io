66a3582d3199070ee08eea7cdf45bcf95bda4aa6
mVang <meng.vang92@yahoo.com>
2018-11-23 11:30:05 -0600

    about page shortened

--- Bootstrap-Portfolio/assets/javascript/index.js
+++ Bootstrap-Portfolio/assets/javascript/index.js
@@ -1,16 +1,30 @@
 $(document).ready(function () {
-    var greet = "";
-    var greetings = ["Hello!", "Welcome!", "Hi!"];
-    greet = randomGreetings();
+    var greetings = [
+        "make a difference.",
+        "solve a problem.",
+        "improve workflow.",
+        "involve innovation.",
+        "involve new concepts.",
+        "help other people.",
+        "demonstrate my skills."];
+    var greet = randomGreetings();
     if (greet == 1) {
-        $("#hello").html(greetings[0]);
+        $("#hello").append(greetings[0]);
     } else if (greet == 2) {
-        $("#hello").html(greetings[1]);
+        $("#hello").append(greetings[1]);
     } else if (greet == 3) {
-        $("#hello").html(greetings[2]);
+        $("#hello").append(greetings[2]);
+    } else if (greet == 4) {
+        $("#hello").append(greetings[3]);
+    } else if (greet == 5) {
+        $("#hello").append(greetings[4]);
+    } else if (greet == 6) {
+        $("#hello").append(greetings[5]);
+    } else if (greet == 7) {
+        $("#hello").append(greetings[6]);
     };
     function randomGreetings() {
-        var greet = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+        var greet = Math.floor(Math.random() * (7 - 1 + 1)) + 1;
         return greet;
     }
 });
