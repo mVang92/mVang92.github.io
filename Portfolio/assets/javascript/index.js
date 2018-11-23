a7825800a438d5821444c533c4dd218e610999ae
mVang <meng.vang92@yahoo.com>
2018-11-23 15:30:52 -0600

    underline css added, about page updated with links

--- Portfolio/assets/javascript/index.js
+++ Portfolio/assets/javascript/index.js
@@ -9,19 +9,19 @@ $(document).ready(function () {
         "demonstrate my skills."];
     var greet = randomGreetings();
     if (greet == 1) {
-        $("#hello").append(greetings[0]);
+        $("#phrase").append(greetings[0]);
     } else if (greet == 2) {
-        $("#hello").append(greetings[1]);
+        $("#phrase").append(greetings[1]);
     } else if (greet == 3) {
-        $("#hello").append(greetings[2]);
+        $("#phrase").append(greetings[2]);
     } else if (greet == 4) {
-        $("#hello").append(greetings[3]);
+        $("#phrase").append(greetings[3]);
     } else if (greet == 5) {
-        $("#hello").append(greetings[4]);
+        $("#phrase").append(greetings[4]);
     } else if (greet == 6) {
-        $("#hello").append(greetings[5]);
+        $("#phrase").append(greetings[5]);
     } else if (greet == 7) {
-        $("#hello").append(greetings[6]);
+        $("#phrase").append(greetings[6]);
     };
     function randomGreetings() {
         var greet = Math.floor(Math.random() * (7 - 1 + 1)) + 1;
