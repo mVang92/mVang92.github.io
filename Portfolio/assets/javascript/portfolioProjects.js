2cbd027281b8347dba464fe34931f88f09352247
mVang <meng.vang92@yahoo.com>
2018-12-18 15:40:01 -0600

    all projects rendering okay with loops, needs minor work

--- Portfolio/assets/javascript/portfolioProjects.js
+++ Portfolio/assets/javascript/portfolioProjects.js
@@ -6,93 +6,16 @@ document.write('\
         <button id="groupProjects" class="btn" data-toggle="collapse" href=".groupCollapse" role="button" aria-expanded="false">Group Projects</button>\
     </div>\
     <!-- Project Row 1 -->\
-    <div class="row text-center box collapse multi-collapse projectCollapse">\
+    <div class="text-center box collapse multi-collapse projectCollapse">\
+        <div id="iProjects"></div>\
         <!-- Project 1 -->\
-        <div id="target1" class="col-md-4 cards" data-toggle="modal" data-target="">\
-            <div class="row">\
-                <div class="col-md-12">\
-                    <div id="project1" class="col-md-12 cardProjectName"></div>\
-                </div>\
-            </div>\
-            <div class="row">\
-                <div class="col-md-12">\
-                    <img id="projectImg1" class="projectImg">\
-                </div>\
-            </div>\
-        </div>\
         <!-- Project 2 -->\
-        <div id="target2" class="col-md-4 cards" data-toggle="modal" data-target="">\
-            <div class="row">\
-                <div id="project2" class="col-md-12 cardProjectName"></div>\
-            </div>\
-            <div class="row">\
-                <div class="col-md-12">\
-                    <img id="projectImg2" class="projectImg">\
-                </div>\
-            </div>\
-        </div>\
         <!-- Project 3 -->\
-        <div id="target3" class="col-md-4 cards" data-toggle="modal" data-target="">\
-            <div class="row">\
-                <div id="project3" class="col-md-12 cardProjectName"></div>\
-            </div>\
-            <div class="row">\
-                <div class="col-md-12">\
-                    <img id="projectImg3" class="projectImg">\
-                </div>\
-            </div>\
-        </div>\
     </div>\
-    <!-- Project Row 2 -->\
-    <div class="row text-center cardBackGround collapse multi-collapse projectCollapse">\
-        <!-- Project 4 -->\
-        <div id="target4" class="col-md-4 cards" data-toggle="modal" data-target="">\
-            <div class="row">\
-                <div id="project4" class="col-md-12 cardProjectName"></div>\
-            </div>\
-            <div class="row">\
-                <div class="col-md-12">\
-                    <img id="projectImg4" class="projectImg">\
-                </div>\
-            </div>\
-        </div>\
-        <!-- Project 5 -->\
-        <div id="target5" class="col-md-4 cards" data-toggle="modal" data-target="">\
-            <div class="row">\
-                <div id="project5" class="col-md-12 cardProjectName"></div>\
-            </div>\
-            <div class="row">\
-                <div class="col-md-12">\
-                    <img id="projectImg5" class="projectImg">\
-                </div>\
-            </div>\
-        </div>\
-        <!-- Project 6 -->\
-        <div id="target6" class="col-md-4 cards" data-toggle="modal" data-target="">\
-            <div class="row">\
-                <div id="project6" class="col-md-12 cardProjectName"></div>\
-            </div>\
-            <div class="row">\
-                <div class="col-md-12">\
-                    <img id="projectImg6" class="projectImg">\
-                </div>\
-            </div>\
-        </div>\
-    </div>\
-    \
     <!-- Group Project Row 1 -->\
-    <div class="row text-center cardBackGround collapse multi-collapse groupCollapse">\
+    <div class="text-center cardBackGround collapse multi-collapse groupCollapse">\
+        <div id="gProjects"></div>\
         <!-- Project 1 -->\
-        <div id="gTarget1" class="col-md-4 cards" data-toggle="modal" data-target="">\
-            <div class="row">\
-                <div id="gProject1" class="col-md-12 cardProjectName"></div>\
-            </div>\
-            <div class="row">\
-                <div class="col-md-12">\
-                    <img id="gProjectImg1" class="projectImg">\
-                </div>\
-            </div>\
-        </div>\
         <!-- Project 2 -->\
         <!-- <div class="col-md-4 cards" data-toggle="modal" data-target="#">\
             \
--- Portfolio/assets/javascript/projectInfo.js
+++ Portfolio/assets/javascript/projectInfo.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Individual projects information
     var projects = [
         {
             title: "Astronomy Trivia",
@@ -38,6 +39,7 @@ $(document).ready(function () {
         }
     ];
 
+    // Group project information
     var groupProjects = [
         {
             title: "BWCA Advanture Tours",
@@ -47,29 +49,56 @@ $(document).ready(function () {
         }
     ];
 
-    // For individual projects
-    for (var i = 0; i < projects.length; i++) {
-        var title = projects[i].title;
-        var img = projects[i].projectImg;
-        var alt = projects[i].alt;
-        var target = projects[i].target;
+    function createCard() {
+        // Create the cards first for individual projects...
+        for (var i = 0; i < projects.length; i++) {
+            var iCounter = i + 1;
+            var iCard = "<div id='target" + iCounter + "' class='col-md-4 cards' data-toggle='modal' data-target=''>"
+            + "<div class='row'><div class='col-md-12'><div id='project" + iCounter + "' class='col-md-12 cardProjectName'></div></div></div>"
+            + "<div class='row'><div class='col-md-12'><img id='projectImg" + iCounter + "' class='projectImg'></div></div></div>";
+            $("#iProjects").append(iCard);
+        };
 
-        $("#project" + (1 + i)).html(title);
-        $("#projectImg" + (1 + i)).attr("src", img);
-        $("#projectImg" + (1 + i)).attr("alt", alt);
-        $("#target" + (1 + i)).attr("data-target", target);
+        // ...then group projects
+        for (var j = 0; j < groupProjects.length; j++) {
+            var gCounter = j + 1;
+            var gCard = "<div id='gTarget" + gCounter + "' class='col-md-4 cards' data-toggle='modal' data-target=''>"
+            + "<div class='row'><div class='col-md-12'><div id='gProject" + gCounter + "' class='col-md-12 cardProjectName'></div></div></div>"
+            + "<div class='row'><div class='col-md-12'><img id='gProjectImg" + gCounter + "' class='projectImg'></div></div></div>";
+            $("#gProjects").append(gCard);
+        };
+
+        getIndividualProjects();
+        getGroupProjects();
+    };
+
+    // For individual projects
+    function getIndividualProjects() {
+        for (var i = 0; i < projects.length; i++) {
+            var title = projects[i].title;
+            var img = projects[i].projectImg;
+            var alt = projects[i].alt;
+            var target = projects[i].target;
+            $("#project" + (1 + i)).html(title);
+            $("#projectImg" + (1 + i)).attr("src", img);
+            $("#projectImg" + (1 + i)).attr("alt", alt);
+            $("#target" + (1 + i)).attr("data-target", target);
+        };
     };
 
     // For group projects
-    for (var j = 0; j < groupProjects.length; j++) {
-        var gTitle = groupProjects[j].title;
-        var gImg = groupProjects[j].projectImg;
-        var gAlt = groupProjects[j].alt;
-        var gTarget = groupProjects[j].target;
+    function getGroupProjects() {
+        for (var j = 0; j < groupProjects.length; j++) {
+            var gTitle = groupProjects[j].title;
+            var gImg = groupProjects[j].projectImg;
+            var gAlt = groupProjects[j].alt;
+            var gTarget = groupProjects[j].target;
+            $("#gProject" + (1 + j)).html(gTitle);
+            $("#gProjectImg" + (1 + j)).attr("src", gImg);
+            $("#gProjectImg" + (1 + j)).attr("alt", gAlt);
+            $("#gTarget" + (1 + j)).attr("data-target", gTarget);
+        };
+    };
 
-        $("#gProject" + (1 + j)).html(gTitle);
-        $("#gProjectImg" + (1 + j)).attr("src", gImg);
-        $("#gProjectImg" + (1 + j)).attr("alt", gAlt);
-        $("#gTarget" + (1 + j)).attr("data-target", gTarget);
-    }
+    createCard();
 });
