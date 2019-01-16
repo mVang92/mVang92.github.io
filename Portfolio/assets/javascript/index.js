573e35b01357df126dea57fc86ea83a35de6e2c8
mVang <meng.vang92@yahoo.com>
2019-01-16 17:54:01 -0600

    cleanup, text-mute footer

--- Portfolio/assets/javascript/index.js
+++ Portfolio/assets/javascript/index.js
@@ -29,10 +29,9 @@ $(document).ready(function () {
 
     function scrollSpy() {
         // Add scrollspy to <body>
-        $('body').scrollspy({ target: ".navbar", offset: 50 });
+        $("body").scrollspy({ target: ".navbar", offset: 50 });
         // Add smooth scrolling on all links inside the navbar
-        $("a").on('click', function (event) {
-
+        $("a").on("click", function (event) {
             // Make sure this.hash has a value before overriding default behavior
             if (this.hash !== "") {
                 event.preventDefault();
@@ -40,7 +39,7 @@ $(document).ready(function () {
                 var hash = this.hash;
                 // Using jQuery's animate() method to add smooth page scroll
                 // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-                $('html, body').animate({
+                $("html, body").animate({
                     scrollTop: $(hash).offset().top
                 }, 500, function () {
                     // Add hash (#) to URL when done scrolling (default click behavior)
