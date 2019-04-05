7dbae9d75dd3191ac973d6b79c0b8a6c5be79057
Meng Vang <meng.vang92@yahoo.com>
2019-04-05 11:56:26 -0500

    updated nyt scrubber app on portfolio. renew API key, refactor

--- nyt-react-search/client/src/utils/API.js
+++ nyt-react-search/client/src/utils/API.js
@@ -2,31 +2,31 @@ import axios from "axios";
 
 export default {
   // Get all of the articles from the database
-  getArticles: function () {
+  getArticles: () => {
     return axios.get("/api/articles");
   },
   // Take the topic and years from the user and put them in the queryURL with API key
-  searchArticle: function (topic, startYear, endYear) {
-    const apiKey = "03401e1f08b24aa185b5bb4d3771714c";
+  searchArticle: (topic, startYear, endYear) => {
+    const apiKey = "M263NCR10mhAsJI7LAG0GM570a7ILBmY";
     const queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=${apiKey}&q=${topic}&begin_date=${startYear}0101&end_date=${endYear}0101`;
     return axios.get(queryUrl);
   },
   // Take the title, url, and date and post them to the database to save
-  saveArticle: function (articleData) {
-    console.log(articleData)
+  saveArticle: articleData => {
+    // console.log(articleData)
     return axios.post("/api/articles/", {
       title: articleData.articleData.headline.main,
       url: articleData.articleData.web_url,
       date: articleData.articleData.pub_date
     })
-      .then(function (response) {
+      .then(response => {
       })
-      .catch(function (error) {
+      .catch(error => {
         console.log(error);
       });
   },
   // Removes the selected article from the database
-  deleteArticle: function (articleData) {
+  deleteArticle: articleData => {
     return (
       axios.delete(`/api/articles/${articleData}`)
     )
